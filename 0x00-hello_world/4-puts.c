1eeb640f7b8607132f307b5e2a8f3782c703de61
gafar537 <gafar.adegok@gmail.com>
2022-12-09 01:02:45 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - print a line of code using puts 
+ * return: 0
+ */
+int main(void)
+{
+	char words[] = "Programming is like building a multilingual puzzle;
+       	puts(words);
+
+	return 0;
+}
+
