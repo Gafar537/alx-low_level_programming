d8078ed54eed96faec0fdad2419b2afc516a40db
gafar537 <gafar.adegok@gmail.com>
2022-12-12 05:38:56 -0800

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,14 +5,14 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-		char letter;
+	char letter;
 
-		for (letter = 'z'; letter >= 'a'; letter--)
-				putchar(letter);
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
 
-		putchar('\n');
-
-		return (0);
+	putchar('\n');
+	return (0);
 }
