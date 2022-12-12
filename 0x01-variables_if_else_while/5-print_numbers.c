7d42d86ee2a2bec59ef64090820cd090cc49ff6c
gafar537 <gafar.adegok@gmail.com>
2022-12-12 04:13:43 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,17 +3,15 @@
 /**
  * main - print single number of base ten
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 
 	for (n = 0; n < 10; n++)
-	{
 		printf("%d, n");
-	}
-	
+
 	printf("\n");
 	
 	return (0);
